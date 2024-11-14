d75e476e42c42b95057de5494b452817e0f2e76e
razan <razanrazanrish@gmail.com>
2024-11-14 16:04:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavbarBasic from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+const App = () => {
+  return (
+    <div>
+      <NavbarBasic/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
